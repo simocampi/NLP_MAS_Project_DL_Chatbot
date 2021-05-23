49e86b3dad08a96c3c06e2ff2f71895a02a16d3f
Simone Campisi <37673011+simocampi@users.noreply.github.com>
2021-05-23 19:04:34 +0200

    GUI, send

--- GUI/gui.py
+++ GUI/gui.py
@@ -2,6 +2,8 @@ from tkinter import *
 import tkinter.ttk as ttk
 from PIL import Image, ImageTk
 
+BACKGROUND_COLOR = "white"
+
 
 class ChatBotGUI(Tk):
 
@@ -10,10 +12,18 @@ class ChatBotGUI(Tk):
         self.title("DeepChatbot")
         self.geometry("400x500")
         self.resizable(width=FALSE, height=FALSE)
+        self.configure(bg=BACKGROUND_COLOR)
+
+        # add iconphoto
+        self.bot_photo = PhotoImage(file="icons/bot.png")
+        self.iconphoto(False, self.bot_photo)
 
         self.click_btn = PhotoImage(file='icons/send_btn.png')
         # Create Chat window
-        self.chat_window = Text(self, bd=0, bg="white", height="8", width="50", font="Arial", )
+        self.chat_window = Text(self, bd=0, highlightthickness=1, highlightcolor="#E5E7E8",
+                                highlightbackground="#E5E7E8", relief="solid", bg="white", height="8",
+                                width="50", font="Calibri")
+
         self.chat_window.config(state=DISABLED)
 
         # Bind scrollbar to Chat window
@@ -29,21 +39,41 @@ class ChatBotGUI(Tk):
 
         # Place all components on the screen
         self.scrollbar.place(x=376, y=6, height=386)
-        self.chat_window.place(x=6, y=6, height=386, width=370)
+        self.chat_window.place(x=9, y=50, height=350, width=345)
         self.entry_box.place(x=9, y=420, height=60, width=345)
         self.send_button.place(x=360, y=440, height=25, width=25)
 
     def send(self):
-        # TODO:to be implemented yet
-        pass
+        message = self.entry_box.get("1.0", 'end-1c').strip()
+        self.entry_box.delete("0.0", END)
+
+        if message != '':
+
+            self.chat_window.tag_config('you', foreground="red", background="#ECF6FF")
+            self.chat_window.tag_config('you2', foreground="black", background="#ECF6FF")
+            self.chat_window.tag_config('bot', foreground="Blue", background="#D7ECFF")
+            self.chat_window.tag_config('bot2', foreground="Black", background="#D7ECFF")
+
+            self.chat_window.config(foreground="#442265", font=("Calibri", 11))
+
+            self.chat_window.config(state=NORMAL)
+            self.chat_window.insert(END, "YOU:  ", "you")
+            self.chat_window.insert(INSERT, message + '\n\n', "you2")
+
+            # res = chatbot_response(msg)
+            res = "Answering To Be implemented yet!!"
+            self.chat_window.insert(END, "BOT:  ", "bot")
+            self.chat_window.insert(INSERT, "bot" + res + '\n\n', "bot2")
+
+            self.chat_window.config(state=DISABLED)
+            self.chat_window.yview(END)
 
     def create_send_button(self):
-
-
-        return Button(master=self, image=self.click_btn, height=30, width=30, command=self.send, relief="flat")
+        return Button(master=self, image=self.click_btn, height=30, width=30, command=self.send, relief="flat",
+                      highlightthickness=0, bd=0, bg=BACKGROUND_COLOR, activebackground=BACKGROUND_COLOR)
 
     def create_entry_box(self):
-        return Text(self, bd=0, bg="white", width="29", height="5", font="Arial")
+        return Text(self, bd=0, bg="#ECF6FF", width="29", height="5", font="Arial")
 
     def run(self):
         self.mainloop()
