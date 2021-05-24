cc2a68438e0d4c3dd0e2a5f09bef3e62fcc617f8
Simone Campisi <37673011+simocampi@users.noreply.github.com>
2021-05-24 17:56:04 +0200

    Lemmatization

---
+++ network/data_preprocessing.py
@@ -0,0 +1,58 @@
+import json
+import numpy as np
+import nltk
+from sklearn.preprocessing import LabelEncoder
+from nltk.stem import WordNetLemmatizer
+
+# TODO: Lemmatizer? Stemming?
+
+# TODO: LabelEncoder ??
+
+# TODO: Tokenization: extract word --> nltk.tokenize
+
+
+"""preprocessing in extracted words: lower case, lemmatization, remove duplicate, remove punctuation mark"""
+
+
+def lemmatize_words(words_list):
+    lemmatizer = WordNetLemmatizer()
+    """Filter punctuations marks"""
+    words_list = filter(lambda w: w != '?', words_list)
+    words_list = filter(lambda w: w != '!', words_list)
+    """Lemmatize words"""
+    lemmatized_words = [lemmatizer.lemmatize(w.lower()) for w in words_list]
+    """remove duplicates"""
+    return sorted(list(dict.fromkeys(lemmatized_words)))
+
+
+"""
+load the dataset, intents.json and make preprocessing on data
+like tokenization 
+"""
+
+
+# TODO: VALUATE TOKENIZATION KERAS
+def load_data(json_intents_filename):
+    data = open(json_intents_filename)
+
+    documents = []
+    words_list = []
+    classes = []
+    responses = []
+
+    for intent in data['intents']:
+        for pattern in intent['patterns']:
+            # use tokenization to extract words from a given pattern
+            extracted_words = nltk.word_tokenize(pattern)
+            # build list of the words
+            words_list.extend(extracted_words)
+            # save couple (list)
+            documents.append((extracted_words, intent['tag']))
+
+        responses.append(intent['responses'])
+
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+    words_training = lemmatize_words()
+    classes = sorted(classes)
