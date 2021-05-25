ff0d61d42bfa05514035c21d808eab6c3f9027ee
Simone Campisi <37673011+simocampi@users.noreply.github.com>
2021-05-25 18:01:04 +0200

    DNN model started

--- network/network.py
+++ network/network.py
@@ -3,9 +3,17 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 
 
+# TODO: se c'è tempo provare con RNN o LSTM
 class ChatbotDNN:
 
-    def __init__(self):
+    def __init__(self, x_train, y_train):
+
+        self.model = Sequential()
+        self.model.add(Dense(128, input_shape=(x_train.shape[1],), activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(y_train.shape[1], activation='softmax'))
         # TODO: Create the model
         pass
 
